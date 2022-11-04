d272a25f0124533a2fd974dbc4ca8982ba90650c
Leslie Low <leslielowgy@gmail.com>
2022-11-04 15:52:35 +0800

    fixed redis

--- project_backend/src/main/java/project_backend/config/RedisConfig.java
+++ project_backend/src/main/java/project_backend/config/RedisConfig.java
@@ -1,24 +1,72 @@
 package project_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
 @Configuration
 @EnableCaching
+@Scope("singleton")
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Bean
-    public LettuceConnectionFactory lettuceConn(){
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+    JedisConnectionFactory jedisConnectionFactory() {
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPassword(redisPassword);
+        redisStandaloneConfiguration.setPort(12122);
+
+
+//        JedisPoolConfig poolConfig = new JedisPoolConfig();
+//        poolConfig.setMaxTotal(128);
+//        poolConfig.setMaxIdle(128);
+//        poolConfig.setMinIdle(16);
+
+
+        JedisClientConfiguration jedisClient = JedisClientConfiguration.builder()
+                .build();
+
+        JedisConnectionFactory jedisConFactory
+                = new JedisConnectionFactory(redisStandaloneConfiguration, jedisClient);
+        jedisConFactory.afterPropertiesSet();
+
+
+        return jedisConFactory;
     }
+
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(){
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(lettuceConn());
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
         return redisTemplate;
     }
+
+
+
+
+
 
 }
